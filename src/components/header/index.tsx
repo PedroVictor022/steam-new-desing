3c2b08f25df639a72e85ebc7c84dfdd8d496af87
Pedro Victor <pedro20002victor@gmail.com>
2023-02-14 08:35:08 -0300

    up

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,19 @@
 import motion from "framer-motion";
+import { useState } from "react";
+import { FaTimes, FaBars } from "react-icons/fa";
 
 const logo = require("./steamLogo.png");
 const myPhoto = require("./myPhoto.jpg");
+const menuLogo = require("./menu.png");
 
 export const Header = () => {
+  const [menu, setMenu] = useState(false);
+
+  const handleMenu = () => {
+    setMenu(!menu);
+    console.log(menu);
+  };
+
   return (
     <div
       className="flex justify-between p-5 text-white font-medium"
@@ -12,23 +22,43 @@ export const Header = () => {
       {/*TODO: IMPLEMENT MENU */}
 
       <div className="flex gap-5 content-center justify-center ml-2">
-        <img 
-          src={logo} 
-          alt="Steam Logo"
-        />
-        <div className="hidden md:flex gap-5 mt-2.5">
-          <p className="text-blue-600">STORE</p>
-          <p>COMMUNITY</p>
-          <p>CHAT</p>
-          <p>SUPORT</p>
+        <img src={logo} alt="Steam Logo" />
+        <div className="hidden sm:flex gap-5 mt-2.5">
+          <div className="border-b-2 border-blue-500">
+            <p className="text-blue-500 font-bold">STORE</p>
+          </div>
+          <div>
+            <p>COMMUNITY</p>
+          </div>
+          <div>
+            <p>CHAT</p>
+          </div>
+          <div>
+            <p>SUPORT</p>
+          </div>
         </div>
       </div>
-      <div className="mr-2 flex content-center justify-center gap-3">
-        <div className="hidden md:flex gap-5 mt-2.5">
+      <div className="hidden sm:flex mr-2 content-center justify-center gap-3">
+        <div className="hidden sm:flex gap-5 mt-2.5">
           <p>Borges</p>
         </div>
         <img src={myPhoto} alt="Profile photo" className="rounded-lg" />
       </div>
+
+      <div
+        className="mt-2 cursor-pointer sm:hidden"
+        onClick={() => handleMenu()}
+      >
+        {/* <img 
+          src={menuLogo} 
+          alt="Profile photo" 
+          className="mt-3.5"
+          onClick={() => handleMenu()}
+        /> */}
+        {menu ? <FaTimes size={25} /> : <FaBars size={25} />}
+      </div>
+
+      {menu ? <div className="sm:px-4 py-2.5 rounded dark:bg-gray-900">oi</div> : null}
     </div>
   );
 };
